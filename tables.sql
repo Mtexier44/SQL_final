f71074dcb52ff85b690bfed2ad687838ce9129f6
Mtexier44 <maiwenn.texier@ynov.com>
2024-10-24 13:08:18 +0200

    insert + tables

---
+++ tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Employee (
+    EmployeeID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Address VARCHAR(255),
+    Seniority INT,
+    DepartmentID UUID NOT NULL,
+    PositionID UUID NOT NULL,
+    FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
+    FOREIGN KEY (PositionID) REFERENCES Position(PositionID)
+);
+
+CREATE TABLE Departments (
+    DepartmentID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Position (
+    PositionID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    Authority INT,
+    DepChef UUID NOT NULL,
+    Salary INT,
+    FOREIGN KEY (DepChef) REFERENCES Position(PositionID) -- Assurez-vous que DepChef fait référence à une Position existante
+);
